d371d5f5d1f0f570cd19c04f4b6470b41a750b18
Arpit Sharma <technoarpitorignal@gmail.com>
2020-11-28 19:37:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import compiler
+import sys
+
+compileFile = False
+
+def getFileText(name):
+    nameArray = name.split('.')
+    if nameArray[len(nameArray) - 1] != 'lca':
+        print('INVALID FILE EXTENTION')
+        return None
+    f = open(name, 'r')
+    text = f.read()
+    return text
+
+while True and not compileFile:
+    if len(sys.argv) < 2:
+        text = input('basic > ')
+        if text.strip() == "quit()": break
+        if text.strip() == "": continue
+    else:
+        compileFile = True
+        text = getFileText(sys.argv[1])
+    if text != None:
+        result, error = compiler.run('<stdin>', text)
+
+        if error:
+            print(error.as_string())
